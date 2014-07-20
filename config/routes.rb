818e880c05010a4dcc2fcdfc9efa72254428301f
Wendy Brannon <wbsolutionsllc2@gmail.com>
2014-07-20 11:27:40 -0600

    In routes.rb 'welcome#time' has been changed to 'time#time'

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'hello' => 'welcome#hello'
   get 'hello/:introduce' => 'welcome#hello'
-  get 'time/now' => 'welcome#time'
+  get 'time/now' => 'time#time'
   get 'introduce/:name1/and/:name2' => 'introduce#introduce'
 end
